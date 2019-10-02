60cf59e7106535f843fbc6383ad9928f3bd6d539
pramod2310 <55196798+pramod2310@users.noreply.github.com>
2019-10-02 00:10:12 -0400

    TraingleSolver
    new dtatic method TraingleSolver Created for main program

---
+++ TriangleSolver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENG8040Assignment2
+{
+    public class TriangleSolver
+    {
+
+        public static string Analyze(int sideA, int sideB ,int sideC)
+        {
+
+            if ((sideA + sideB) >= (sideC) && (sideA + sideC) >= (sideB) && (sideB + sideC) >= sideA)
+            {
+
+                if (sideA == sideB && sideB == sideC)
+                {
+                    return ("This is a Equilateral Triangle.");
+                }
+                else if (sideA == sideB || sideA == sideC || sideB == sideC)
+                {
+                    return ("This is a Isocels Triangle.");
+                }
+                else
+                {
+                    return ("This is a Scalene Triangle.");
+                }
+            }
+            else
+            {
+                return ("Triangle is Not Posibble.");
+            }
+
+        }
+    }
+}
